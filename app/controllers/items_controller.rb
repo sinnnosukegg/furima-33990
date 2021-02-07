0ad6647a923c038cab1b7f49e466e88f1bb99a9a
sinnnosukegg <shinnosukegg@icloud.com>
2021-02-07 20:30:59 +0900

    syuusei

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    #@items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
